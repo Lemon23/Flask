db05961aea082a28aa900b1188ef13d80fa44b9b
System Administrator <root@liuqiaodeMacBook-Pro.local>
2016-01-24 14:26:48 +0800

    Performance

--- app/main/views.py
+++ app/main/views.py
@@ -2,12 +2,24 @@ from datetime import datetime
 from flask import render_template, session, redirect, url_for, \
         abort, flash, request, current_app, make_response
 from flask.ext.login import login_required, current_user
+from flask.ext.sqlalchemy import get_debug_queries
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm, \
         CommentForm
 from .. import db
 from ..models import User, Role, Post, Permission, Comment
 from ..decorators import admin_required, permission_required
+
+# Report slow database queries
+@main.after_app_request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+                % (query.statement, query.parameters, query.duration,
+                   query.context))
+    return response
 
 # Close server routing
 @main.route('/shutdown')
